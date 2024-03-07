16569f7c34506eaa88b52d6a0370cc1fd728330e
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-03-07 15:18:01 -0700

    Add :UseCustomRT() and :DrawRT()

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 These can be arbitrarily resized through the context menu, given materials and colors via toolgun, and the context menu allows for url images to be used.
 
-Materials will be remade as fullbright when displayed as quad props. Because glua cannot read material proxies, this means animated materials are unsupported. However, developers can use `:SetCustomMaterial( IMaterial )` clientside to use their own materials, allowing for custom-made animated materials or rendertarget-driven animations.
+Materials will be remade as fullbright when displayed as quad props. Because glua cannot read material proxies, this means animated materials are unsupported.
+However, developers can use `:SetCustomMaterial( mat )` clientside to use their own material objects, allowing for custom-made animated materials or rendertarget-driven animations.
+Developers can also use `:UseCustomRT( rtName, rtWidth, rtHeight )` and override the quad prop's `:DrawRT()` function for even easier RT drawing.
 
 Per-player quad_prop limits can be set with the `sbox_maxquad_prop` serverside convar.
--- lua/entities/quad_prop/cl_init.lua
+++ lua/entities/quad_prop/cl_init.lua
@@ -7,8 +7,10 @@ ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
 
 local entMeta = nil
 local classMeta = nil
+local rtIncr = 0
 
 local wrapMeta
+local isRTInUse
 
 
 function ENT:Initialize()
@@ -30,7 +32,15 @@ function ENT:SetMaterial( path )
 end
 
 function ENT:_SetMaterial( path )
+    local rtName = self._rtName
+
+    if rtName then
+        hook.Remove( "PreRender", "QuadProps_DrawCustomRT_" .. rtName )
+    end
+
     self._usingCustomMaterial = false
+    self._usingCustomRT = false
+    self._rtName = nil
     self._materialObject = QuadProps.AcquireMaterial( self, path or "" )
 end
 
@@ -61,6 +71,82 @@ function ENT:IsUsingCustomMaterial()
     return self._usingCustomMaterial
 end
 
+--[[
+    - Sets up this quad prop with a rendertarget, accompanying material, and PreRender hook for custom RT drawing.
+    - Override this entity's :DrawRT() function to draw to the RT.
+
+    rtName: (optional) (string)
+        - The name of the RT. Does nothing if it's the same as the current RT name.
+        - Must be different from the RT name of any other quad prop.
+        - If not provided, a unique name will be generated.
+            - Old auto-generated RTs will not be reused, thus creating a memory leak as RTs are not garbage collected.
+            - It is HIGHLY recommended to provide a name to avoid said leak, or guarantee this RT gets used permanently.
+    rtWidth: (optional) (number)
+        - The width of the RT. Defaults to 1024.
+        - If not a power of 2, will become the nearest power of 2, as per GetRenderTarget()'s behavior.
+    rtHeight: (optional) (number)
+        - The height of the RT. Defaults to rtWidth.
+--]]
+function ENT:UseCustomRT( rtName, rtWidth, rtHeight )
+    rtWidth = rtWidth or 1024
+    rtHeight = rtHeight or rtWidth
+
+    if not rtName then
+        rtIncr = rtIncr + 1
+        rtName = "quad_prop_rt_" .. rtIncr
+    end
+
+    if self._rtName == rtName then return end
+
+    if isRTInUse( rtName ) then
+        error( "RT name already in use", 1 )
+    end
+
+    self:SetMaterial( "" )
+
+    local hookName = "QuadProps_DrawCustomRT_" .. rtName
+    local rt = GetRenderTarget( rtName, rtWidth, rtHeight )
+    local mat = CreateMaterial( rtName, "UnlitGeneric", {
+        ["$vertexcolor"] = 1,
+        ["$vertexalpha"] = 1,
+    } )
+
+    mat:SetTexture( "$basetexture", rt )
+
+    self._usingCustomRT = true
+    self._rtName = rtName
+    self._materialObject = mat
+
+    hook.Add( "PreRender", hookName, function()
+        local drawRT = self.DrawRT
+
+        -- No longer a valid entity. Since quad props always transmit, forceupdate won't cause this to trigger prematurely.
+        if not drawRT then
+            hook.Remove( "PreRender", hookName )
+
+            return
+        end
+
+        local oldW, oldH = ScrW(), ScrH()
+
+        render.PushRenderTarget( rt )
+            render.SetViewPort( 0, 0, rtWidth, rtHeight )
+            cam.Start2D()
+            drawRT( self )
+            cam.End2D()
+            render.SetViewPort( 0, 0, oldW, oldH )
+        render.PopRenderTarget()
+    end )
+end
+
+function ENT:IsUsingCustomRT()
+    return self._usingCustomRT
+end
+
+function ENT:GetRTName()
+    return self._rtName
+end
+
 function ENT:Draw()
     self:DrawQuad()
 end
@@ -81,6 +167,17 @@ function ENT:DrawQuad()
 
     render.SetMaterial( mat )
     render.DrawQuadEasy( pos, normal, width, height, color, roll )
+end
+
+
+----- OVERRIDEABLE FUNCTIONS -----
+
+--[[
+    - Called during PreRender for quad props using a custom rendertarget.
+    - This will be a 2D rendering context with the quad prop's RT already selected.
+--]]
+function ENT:DrawRT()
+
 end
 
 
@@ -121,3 +218,16 @@ wrapMeta = function( quadProp )
     debug.setmetatable( quadProp, meta )
 end
 
+isRTInUse = function( rtName )
+    local quadProps = ents.FindByClass( "quad_prop" )
+
+    -- Check manually every time instead of tracking manually to avoid potential miscounts (lua autorefresh, EntityRemoved hook breaking from an addon, etc)
+    -- The added perf cost is a non-issue since this function will be very rarely called.
+    for _, quadProp in ipairs( quadProps ) do
+        if quadProp:GetRTName() == rtName then
+            return true
+        end
+    end
+
+    return false
+end
