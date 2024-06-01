98658fad3bd3706291bb7a49f29030348e251e37
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-06-01 16:55:37 -0500

    Improve right click mode

--- lua/weapons/gmod_tool/stools/quad_prop_maker.lua
+++ lua/weapons/gmod_tool/stools/quad_prop_maker.lua
@@ -9,6 +9,8 @@ TOOL.Information = {
     { name = "left_1_use", stage = 1, op = 1 },
     { name = "right_0", stage = 0, op = 0 },
     { name = "right_1", stage = 1, op = 2 },
+    { name = "right_1_use", stage = 1, op = 2 },
+    { name = "right_2", stage = 2, op = 2 },
     { name = "reload" },
 }
 
@@ -20,7 +22,9 @@ if CLIENT then
     language.Add( "tool.quad_prop_maker.left_1", "Select the second corner" )
     language.Add( "tool.quad_prop_maker.left_1_use", "Select the second corner (rotates with your aim, use on floors/ceilings)" )
     language.Add( "tool.quad_prop_maker.right_0", "Select the first corner (aligns vertically)" )
-    language.Add( "tool.quad_prop_maker.right_1", "Select the second corner" )
+    language.Add( "tool.quad_prop_maker.right_1", "Determine the width" )
+    language.Add( "tool.quad_prop_maker.right_1_use", "Determine the width (ignore line of sight)" )
+    language.Add( "tool.quad_prop_maker.right_2", "Determine the height" )
     language.Add( "tool.quad_prop_maker.reload", "Remove a quad prop" )
 
     language.Add( "tool.quad_prop_maker.wall_dist", "Wall Distance" )
@@ -29,8 +33,20 @@ end
 
 
 local VECTOR_ZERO = Vector( 0, 0, 0 )
+local VECTOR_UP = Vector( 0, 0, 1 )
+local VECTOR_UP_LONGER = Vector( 0, 0, 10 )
+local ANGLE_ZERO = Angle( 0, 0, 0 )
 local SIZE_MAX = QuadProps.SIZE_MAX
 local THICKNESS = QuadProps.THICKNESS
+
+local PREVIEW_MAT = CLIENT and Material( "color" )
+local PREVIEW_COLOR_BOX = Color( 255, 255, 255, 50 )
+local PREVIEW_COLOR_BOX_WIREFRAME = Color( 255, 255, 255, 255 )
+local PREVIEW_COLOR_PLANE = Color( 0, 255, 255, 25 )
+local PREVIEW_COLOR_LINE = Color( 255, 255, 0, 255 )
+
+local PREVIEW_PLANE_MINS = Vector( -SIZE_MAX, -SIZE_MAX, 0 )
+local PREVIEW_PLANE_MAXS = Vector( SIZE_MAX, SIZE_MAX, 0 )
 
 local mathAbs = math.abs
 local mathMin = math.min
@@ -38,6 +54,15 @@ local mathMin = math.min
 
 local function mathSign( x )
     return x > 0 and 1 or x < 0 and -1 or 0
+end
+
+local function getTraceStartAndDir( tr )
+    local startPos = tr.StartPos
+    local fromTo = tr.HitPos - startPos
+    local fromToLength = fromTo:Length()
+    if fromToLength == 0 then return end
+
+    return startPos, fromTo / fromToLength
 end
 
 local function lineIntersectPlane( lineOrigin, lineDir, planeOrigin, planeNormal )
@@ -54,6 +79,17 @@ local function lineIntersectPlane( lineOrigin, lineDir, planeOrigin, planeNormal
     local dist = ( planeOrigin - lineOrigin ):Dot( planeNormal ) / lineDotNormal
 
     return lineOrigin + lineDir * dist
+end
+
+local function resetStencil()
+    render.SetStencilWriteMask( 0xFF )
+    render.SetStencilTestMask( 0xFF )
+    render.SetStencilReferenceValue( 0 )
+    render.SetStencilCompareFunction( STENCIL_ALWAYS )
+    render.SetStencilPassOperation( STENCIL_KEEP )
+    render.SetStencilFailOperation( STENCIL_KEEP )
+    render.SetStencilZFailOperation( STENCIL_KEEP )
+    render.ClearStencil()
 end
 
 
@@ -80,13 +116,37 @@ end
 function TOOL:RightClick( tr )
     if self:GetOperation() == 1 then return false end
 
-    if self:GetStage() == 0 then
-        local hitNormal = tr.HitNormal
-        if hitNormal == VECTOR_ZERO then return false end
+    local stage = self:GetStage()
 
-        self:SetObject( 0, game.GetWorld(), tr.HitPos, nil, 0, hitNormal )
+    if stage == 0 then
+        self:SetObject( 0, game.GetWorld(), tr.HitPos, nil, 0, tr.HitNormal )
         self:SetOperation( 2 )
         self:SetStage( 1 )
+    elseif stage == 1 then
+        local eyePos, eyeDir = getTraceStartAndDir( tr )
+        if not eyePos then return false end
+
+        local cornerPos1 = self:GetPos( 0 )
+        local cornerPos3
+
+        if self:GetOwner():KeyDown( IN_USE ) then
+            cornerPos3 = lineIntersectPlane( eyePos, eyeDir, cornerPos1, VECTOR_UP )
+        else
+            local hitPos = tr.HitPos
+
+            cornerPos3 = Vector( hitPos[1], hitPos[2], cornerPos1[3] )
+        end
+
+        local corner1To3 = cornerPos3 - cornerPos1
+        local corner1To3Length = corner1To3:Length()
+        if corner1To3Length < 1 then return false end
+
+        local corner1To3Dir = corner1To3 / corner1To3Length
+        local cornerNormal3 = corner1To3Dir:Angle():Right()
+
+        self:SetObject( 1, game.GetWorld(), cornerPos3, nil, 0, cornerNormal3 )
+        self:SetOperation( 2 )
+        self:SetStage( 2 )
     else
         if CLIENT then return true end
 
@@ -128,7 +188,9 @@ end
 function TOOL:CalcSpawnInfo( tr )
     local op = self:GetOperation()
     if op == 0 then return end
-    if self:GetStage() == 0 then return end
+
+    local stage = self:GetStage()
+    if stage == 0 then return end
 
     tr = tr or self:GetOwner():GetEyeTrace()
 
@@ -138,14 +200,11 @@ function TOOL:CalcSpawnInfo( tr )
     local pos, ang, width, height
     local cornerPos2, corner1To2
 
+    local eyePos, eyeDir = getTraceStartAndDir( tr )
+    if not eyePos then return end
+
     if op == 1 then
         -- Use the first surface to orient the quad
-        local eyePos = tr.StartPos
-        local eyeVec = tr.HitPos - eyePos
-        local eyeVecLength = eyeVec:Length()
-        if eyeVecLength == 0 then return end
-
-        local eyeDir = eyeVec / eyeVecLength
         cornerPos2 = lineIntersectPlane( eyePos, eyeDir, cornerPos1, cornerNormal1 )
         if not cornerPos2 then return end
 
@@ -170,9 +229,30 @@ function TOOL:CalcSpawnInfo( tr )
 
         ang = cornerNormal1:AngleEx( upDir )
     else
-        -- Force the quad to always stand up vertically
-        cornerPos2 = tr.HitPos
-        local cornerNormal2 = tr.HitNormal
+        -- Force the quad to always stand up vertically, using three corners
+        local cornerPos3
+
+        if stage == 1 then
+            if self:GetOwner():KeyDown( IN_USE ) then
+                cornerPos3 = lineIntersectPlane( eyePos, eyeDir, cornerPos1, VECTOR_UP )
+            else
+                local hitPos = tr.HitPos
+
+                cornerPos3 = Vector( hitPos[1], hitPos[2], cornerPos1[3] )
+            end
+
+            if not cornerPos3 then return end
+
+            cornerPos2 = cornerPos3 + VECTOR_UP_LONGER
+        else
+            local cornerNormal3 = self:GetNormal( 1 )
+
+            cornerPos3 = self:GetPos( 1 )
+            local heightPoint = lineIntersectPlane( eyePos, eyeDir, cornerPos3, cornerNormal3 )
+            if not heightPoint then return end
+
+            cornerPos2 = Vector( cornerPos3[1], cornerPos3[2], heightPoint[3] )
+        end
 
         corner1To2 = cornerPos2 - cornerPos1
         local corner1To2Length = corner1To2:Length()
@@ -184,10 +264,10 @@ function TOOL:CalcSpawnInfo( tr )
         local forwardDirA = corner1To2Dir:Angle():Right()
         local forwardDirB = -forwardDirA
 
-        -- Choose a forward based on whichever is more closely aligned with the corner normals.
-        local forwardDirDotA = forwardDirA:Dot( cornerNormal1 ) + forwardDirA:Dot( cornerNormal2 )
-        local forwardDirDotB = forwardDirB:Dot( cornerNormal1 ) + forwardDirB:Dot( cornerNormal2 )
-        local forwardDir = forwardDirDotA >= forwardDirDotB and forwardDirA or forwardDirB
+        -- Choose a forward based on whichever opposes the player's eyes more
+        local forwardDirDotA = forwardDirA:Dot( eyeDir )
+        local forwardDirDotB = forwardDirB:Dot( eyeDir )
+        local forwardDir = forwardDirDotA <= forwardDirDotB and forwardDirA or forwardDirB
 
         ang = forwardDir:Angle()
     end
@@ -233,7 +313,7 @@ function TOOL:MakeQuadProp( tr )
 
     ent:SetSquare( false )
     ent:SetSize( width, height )
-    ent:SetDoubleSided( true )
+    ent:SetDoubleSided( false )
     if CPPI then ent:CPPISetOwner( owner ) end
 
     owner:AddCount( "quad_prop", ent )
@@ -266,12 +346,44 @@ function TOOL:StartPreview()
 
         if selfObj:GetStage() == 0 then return end
 
-        local pos, ang, width, height = selfObj:CalcSpawnInfo()
+        local tr = LocalPlayer():GetEyeTrace()
+        local pos, ang, width, height = selfObj:CalcSpawnInfo( tr )
         if not pos then return end
 
         local maxs = Vector( THICKNESS / 2, width / 2, height / 2 )
 
-        render.DrawWireframeBox( pos, ang, -maxs, maxs, color_white, false )
+        render.SetMaterial( PREVIEW_MAT )
+
+        -- Extra preview helpers
+        if selfObj:GetOperation() == 2 and selfObj:GetStage() == 1 then
+            local cornerPos1 = selfObj:GetPos( 0 )
+
+            render.DrawBox( cornerPos1 + VECTOR_UP, ANGLE_ZERO, PREVIEW_PLANE_MINS, PREVIEW_PLANE_MAXS, PREVIEW_COLOR_PLANE )
+
+            if not LocalPlayer():KeyDown( IN_USE ) then
+                local cornerPos1ToCenter = pos - cornerPos1
+                cornerPos1ToCenter[3] = 0
+                local cornerPos3 = cornerPos1 + cornerPos1ToCenter * 2
+
+                render.DrawLine( tr.HitPos, cornerPos3, PREVIEW_COLOR_LINE, false )
+            end
+        end
+
+        resetStencil()
+
+        render.SetStencilEnable( true )
+        render.SetStencilReferenceValue( 1 )
+        render.SetStencilCompareFunction( STENCIL_ALWAYS )
+        render.SetStencilZFailOperation( STENCIL_REPLACE )
+
+        -- Draw preview
+        render.DrawBox( pos, ang, -maxs, maxs, PREVIEW_COLOR_BOX )
+        render.DrawWireframeBox( pos, ang, -maxs, maxs, PREVIEW_COLOR_BOX_WIREFRAME, false )
+
+        render.SetStencilCompareFunction( STENCIL_EQUAL )
+        render.ClearBuffersObeyStencil( 255, 0, 0, 255, false )
+
+        render.SetStencilEnable( false )
     end )
 end
 
