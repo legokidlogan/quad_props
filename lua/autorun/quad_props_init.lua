db4281c40ba6a9b0530750683c03ee2e1f31067c
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-06-02 09:53:49 -0500

    Fix limit mode minimum

--- lua/autorun/quad_props_init.lua
+++ lua/autorun/quad_props_init.lua
@@ -4,7 +4,7 @@ QuadProps.THICKNESS = 1
 
 
 CreateConVar( "sbox_maxquad_prop", 10, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "The max number of quad props per player.", 1, 1000 )
-CreateConVar( "quad_props_size_limit_mode", 0, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "0 To always enforce size limit, 1 to ignore it on world-owned or superadmin-owned quad props (requires a prop protection addon), 2 to always ignore it.", 1, 2 )
+CreateConVar( "quad_props_size_limit_mode", 0, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "0 To always enforce size limit, 1 to ignore it on world-owned or superadmin-owned quad props (requires a prop protection addon), 2 to always ignore it.", 0, 2 )
 
 if cleanup then
     cleanup.Register( "quad_prop" )
